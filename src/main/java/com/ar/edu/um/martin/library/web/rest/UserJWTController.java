a181df5f7f408d0730b304937ed0b084e59d9c0d
martingreyes <mg.reyes@alumno.um.edu.ar>
2023-12-03 19:23:58 -0300

    ELK

--- src/main/java/com/ar/edu/um/martin/library/web/rest/UserJWTController.java
+++ src/main/java/com/ar/edu/um/martin/library/web/rest/UserJWTController.java
@@ -5,9 +5,12 @@ import com.ar.edu.um.martin.library.security.jwt.TokenProvider;
 import com.ar.edu.um.martin.library.web.rest.vm.LoginVM;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -23,6 +26,8 @@ public class UserJWTController {
 
     private final TokenProvider tokenProvider;
 
+    private final Logger log = LoggerFactory.getLogger(UserJWTController.class);
+
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     public UserJWTController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
@@ -32,17 +37,23 @@ public class UserJWTController {
 
     @PostMapping("/authenticate")
     public ResponseEntity<JWTToken> authorize(@Valid @RequestBody LoginVM loginVM) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-            loginVM.getUsername(),
-            loginVM.getPassword()
-        );
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                loginVM.getUsername(),
+                loginVM.getPassword()
+            );
 
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = tokenProvider.createToken(authentication, loginVM.isRememberMe());
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-        return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String jwt = tokenProvider.createToken(authentication, loginVM.isRememberMe());
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+            log.info("INICIO DE SESIÓN EXITOSO - Usuario: {}, Contraseña: {}", loginVM.getUsername(), loginVM.getPassword());
+            return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+        } catch (BadCredentialsException e) {
+            log.warn("INICIO DE SESIÓN FALLIDO - Usuario: {}, Contraseña: {}", loginVM.getUsername(), loginVM.getPassword());
+            throw e;
+        }
     }
 
     /**
