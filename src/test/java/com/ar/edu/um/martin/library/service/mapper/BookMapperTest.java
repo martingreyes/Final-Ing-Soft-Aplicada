90cf5644156f0ad9f38e7d9e1fe012d862005a7b
martingreyes <mg.reyes@alumno.um.edu.ar>
2023-12-03 19:06:21 -0300

    Test de Unidad

--- src/test/java/com/ar/edu/um/martin/library/service/mapper/BookMapperTest.java
+++ src/test/java/com/ar/edu/um/martin/library/service/mapper/BookMapperTest.java
@@ -1,7 +1,10 @@
 package com.ar.edu.um.martin.library.service.mapper;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.ar.edu.um.martin.library.domain.Book;
+import com.ar.edu.um.martin.library.service.dto.BookDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +15,48 @@ class BookMapperTest {
     @BeforeEach
     public void setUp() {
         bookMapper = new BookMapperImpl();
+    }
+
+    @Test
+    public void testDtoToEntityMapping() {
+        BookDTO bookDTO = new BookDTO();
+        bookDTO.setId(1L);
+        bookDTO.setIsbn("1234");
+        bookDTO.setName("El Principito");
+        bookDTO.setPublishYear("1943");
+        bookDTO.setCopies(5);
+
+        Book book = bookMapper.toEntity(bookDTO);
+
+        assertEquals(book.getId(), bookDTO.getId());
+        assertEquals(book.getIsbn(), bookDTO.getIsbn());
+        assertEquals(book.getName(), bookDTO.getName());
+        assertEquals(book.getPublishYear(), bookDTO.getPublishYear());
+        assertEquals(book.getCopies(), bookDTO.getCopies());
+    }
+
+    @Test
+    public void testEntityToDtoMapping() {
+        Book book = new Book();
+        book.setId(1L);
+        book.setIsbn("1234");
+        book.setName("El Principito");
+        book.setPublishYear("1943");
+        book.setCopies(5);
+
+        BookDTO bookDTO = bookMapper.toDto(book);
+
+        assertEquals(book.getId(), bookDTO.getId());
+        assertEquals(book.getIsbn(), bookDTO.getIsbn());
+        assertEquals(book.getName(), bookDTO.getName());
+        assertEquals(book.getPublishYear(), bookDTO.getPublishYear());
+        assertEquals(book.getCopies(), bookDTO.getCopies());
+    }
+
+    @Test
+    public void testNullDtoToEntityMapping() {
+        BookDTO bookDTO = null;
+        Book book = bookMapper.toEntity(bookDTO);
+        assertNull(book);
     }
 }
--- src/test/java/com/ar/edu/um/martin/library/service/mapper/ClientMapperTest.java
+++ src/test/java/com/ar/edu/um/martin/library/service/mapper/ClientMapperTest.java
@@ -1,7 +1,10 @@
 package com.ar.edu.um.martin.library.service.mapper;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.ar.edu.um.martin.library.domain.Client;
+import com.ar.edu.um.martin.library.service.dto.ClientDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +15,52 @@ class ClientMapperTest {
     @BeforeEach
     public void setUp() {
         clientMapper = new ClientMapperImpl();
+    }
+
+    @Test
+    public void testDtoToEntityMapping() {
+        ClientDTO clientDTO = new ClientDTO();
+        clientDTO.setId(1L);
+        clientDTO.setFirstName("Martin");
+        clientDTO.setLastName("Reyes");
+        clientDTO.setEmail("martin@gmail.com");
+        clientDTO.setAddress("Mendoza, Argentina");
+        clientDTO.setPhone("1234");
+
+        Client client = clientMapper.toEntity(clientDTO);
+
+        assertEquals(clientDTO.getId(), client.getId());
+        assertEquals(clientDTO.getFirstName(), client.getFirstName());
+        assertEquals(clientDTO.getLastName(), client.getLastName());
+        assertEquals(clientDTO.getEmail(), client.getEmail());
+        assertEquals(clientDTO.getAddress(), client.getAddress());
+        assertEquals(clientDTO.getPhone(), client.getPhone());
+    }
+
+    @Test
+    public void testEntityToDtoMapping() {
+        Client client = new Client();
+        client.setId(1L);
+        client.setFirstName("Martin");
+        client.setLastName("Reyes");
+        client.setEmail("martin@gmail.com");
+        client.setAddress("Mendoza, Argentina");
+        client.setPhone("1234");
+
+        ClientDTO clientDTO = clientMapper.toDto(client);
+
+        assertEquals(client.getId(), clientDTO.getId());
+        assertEquals(client.getFirstName(), clientDTO.getFirstName());
+        assertEquals(client.getLastName(), clientDTO.getLastName());
+        assertEquals(client.getEmail(), clientDTO.getEmail());
+        assertEquals(client.getAddress(), clientDTO.getAddress());
+        assertEquals(client.getPhone(), clientDTO.getPhone());
+    }
+
+    @Test
+    public void testNullDtoToEntityMapping() {
+        ClientDTO clientDTO = null;
+        Client client = clientMapper.toEntity(clientDTO);
+        assertNull(client);
     }
 }
